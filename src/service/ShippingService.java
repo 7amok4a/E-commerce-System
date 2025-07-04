21e4657f65be8022e0fd187a08223e69690ec9fb
7amok4a <7amok4a.9960@gmail.com>
2025-07-05 01:14:58 +0300

    changes

--- src/service/ShippingService.java
+++ src/service/ShippingService.java
@@ -7,6 +7,10 @@ import interfaces.Shippable;
 public class ShippingService {
     
     public void methodShipping(List<Shippable> products) {
-        System.out.println();
+        System.out.println("Methods shipping for Products"); 
+        for (Shippable product:products) {
+            System.out.println("Name : " + product.getName() + "Weight " + product.getWeight() );
+        }
     }
 } 
+
